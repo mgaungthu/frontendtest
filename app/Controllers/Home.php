0f0e9abd61caf162a7a334b52448b9fee5d0ff47
server3523 <server3523@gmail.com>
2023-05-09 12:40:43 +0630

    update:

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,7 +7,6 @@ class Home extends BaseController
     public function index()
     {
     	 $result = json_decode(file_get_contents(WRITEPATH."data.json"), true);
-
     	 $data['sub_title'] = $result['sub_title'];
     	 $data['title'] = $result['title'];
        	 return view('main',$data);
