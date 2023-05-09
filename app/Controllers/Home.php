1acaf5dda2b2b1ab22cb7e39beea966686542716
server3523 <server3523@gmail.com>
2023-05-09 12:34:02 +0630

    first commit

---
+++ app/Controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index()
+    {
+    	 $result = json_decode(file_get_contents(WRITEPATH."data.json"), true);
+
+    	 $data['sub_title'] = $result['sub_title'];
+    	 $data['title'] = $result['title'];
+       	 return view('main',$data);
+    }
+}
